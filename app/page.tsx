507bb13fc2c29f7a00a1c1e99608f691894b0285
chongkelv <e0895806@u.nus.edu>
2024-03-25 17:02:51 +0900

    Change the layout of the table

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import PeerprepLogo from '@/app/ui/peerprep-logo';
 import { CreateQuestion, CreateCategory } from '@/app/ui/questions/buttons';
 import Search from '@/app/ui/search';
 import { lusitana } from '@/app/ui/fonts';
-import {QuestionsTable, CategoriesTable} from '@/app/ui/questions/table';
+import { QuestionsTable, CategoriesTable } from '@/app/ui/questions/table';
 
 export default function Page() {
   return (
@@ -10,27 +10,23 @@ export default function Page() {
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-40">
         <PeerprepLogo />
       </div>
-      <div className='w-full col-sm'>
-        <div className='flex w-full items-center justify-between'>
-          <h1 className={`${lusitana.className} text-2x1`}>Questions Table</h1>
+      <div className="mt-4 flex grow flex-col gap-4 md:flex-row">
+        <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
+            <CreateCategory />
+          </div>
+          <CategoriesTable />
         </div>
-        <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
-          {/* <Search placeholder='Search questions...' /> */}
-          <CreateQuestion />
+        <div className="fflex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
+          {/* Add Hero Images Here */}
+          {/*<Search placeholder='Search questions...' />*/}
+          <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>
+            <CreateQuestion />
+          </div>
+          <QuestionsTable />
         </div>
-        <QuestionsTable />
-      </div>
-      <br /><br />
-
-      <div>
-        <div className='flex w-full items-center justify-between'>
-          <h1 className={`${lusitana.className} text-2x1`}>Category Table</h1>
-        </div>
-        <div className='mt-4 flex items-center justify-between gap-2 md:mt-8'>          
-          <CreateCategory />
-        </div>        
-        <CategoriesTable />
       </div>
     </main>
+
   );
 }
