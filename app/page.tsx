9b50037b765a553dd143711f43fa6294b3771a2b
Kelvin <chongkelv@gmail.com>
2024-04-10 11:47:54 +0800

    Added Log In Form

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,8 @@ import { QuestionsTable, CategoriesTable } from '@/app/ui/questions/table';
 // import { categories } from './lib/placeholder-data';
 import { notFound } from 'next/navigation';
 import Image from 'next/image';
-import Form from '@/app/ui/user-account/create-form';
+import CreateAccountForm from '@/app/ui/user-account/create-form';
+import LogInForm from '@/app/ui/user-account/login-form';
 
 
 export default async function Page() {
@@ -30,14 +31,15 @@ export default async function Page() {
           </div>
         </div>
         <div className="fflex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/6 md:px-20">
+        <h1 className="text-[20px]">Log in here</h1>
           <div className="ml-auto flex gap-4">
-            <LogInButton />
+            <LogInForm />
           </div>
         </div>
         <div className="fflex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/6 md:px-20">
           <h1 className="text-[20px]">Create an account here</h1>
           <div className="ml-auto flex gap-4">
-            <Form />
+            <CreateAccountForm />
           </div>
         </div>
       </div>
--- app/ui/user-account/login-form.tsx
+++ app/ui/user-account/login-form.tsx
@@ -32,26 +32,7 @@ export default function Form() {
   return (
     <form> {/* todo: action={handleFormAction} */}
       <div className="rounded-md bg-gray-50 p-4 md:p-6">
-        {/* Username Input */}
-        <div className="mb-4">
-          <label htmlFor="amount" className="mb-2 block text-sm font-medium">
-            Input username here.
-          </label>
-          <div className="relative mt-2 rounded-md">
-            <div className="relative">
-              <input
-                id='username'
-                name='username'
-                type="string"
-                placeholder="Username"
-                className="peer block w-full rounded-md border border-gray-200 py-2 pl-10 text-sm outline-2 placeholder:text-gray-500"
-                required
-              />
-              <UserIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
-            </div>
-          </div>
-        </div>
-
+        
         {/* Email Input */}
         <div className="mb-4">
           <label htmlFor="amount" className="mb-2 block text-sm font-medium">
@@ -93,7 +74,7 @@ export default function Form() {
         </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
-        <CreateAccountButton />
+        <LogInButton />
       </div>
     </form >
   );
