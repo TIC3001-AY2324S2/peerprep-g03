3b0c744aa569f590c753c104e8993d48c71f1c4f
Kelvin <chongkelv@gmail.com>
2024-04-18 15:22:37 +0800

    Add the greeting message to all page

--- project-integrated/app/dashboard/matching/page.tsx
+++ project-integrated/app/dashboard/matching/page.tsx
@@ -1,16 +1,18 @@
-import PeerprepLogo from '@/app/ui/peerprep-logo';
 import { getUsername } from '@/app/lib/action'
-import { PowerIcon } from '@heroicons/react/24/outline';
 import MathchingForm from '@/app/ui/matching/find-match-form';
 import { cookies } from 'next/headers'
-import Link from 'next/link';
+import { lusitana } from '@/app/ui/fonts';
 
 export default async function Page() {
   const token = cookies().get('accessToken')?.value
   const username = await getUsername(token)
 
   return (
-    <main className="flex min-h-screen flex-col p-6">  
+    <main className="flex min-h-screen flex-col p-6">
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Matching
+      </h1>
+      <p className="text-lg font-bold">Welcome, {username}!</p>
       <div className="mt-4 flex grow flex-col gap-4 md:flex-row">        
         <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-3/5 md:px-20">          
           <h1 className="text-[20px] self-center">Find Matching here</h1>
--- project-integrated/app/dashboard/questions/page.tsx
+++ project-integrated/app/dashboard/questions/page.tsx
@@ -2,12 +2,15 @@ import { CreateQuestion, CreateCategory } from '@/app/ui/questions/buttons';
 import { QuestionsTable, CategoriesTable } from '@/app/ui/questions/table';
 import { fetchQuestions, fetchCategories } from '@/app/lib/data';
 import { notFound } from 'next/navigation';
-
+import { cookies } from 'next/headers'
+import { getUsername } from '@/app/lib/action'
+import { lusitana } from '@/app/ui/fonts';
 
 export default async function Page() {
   const questions = await fetchQuestions();
   const categories = await fetchCategories();
-  
+  const token = cookies().get('accessToken')?.value
+  const username = await getUsername(token)
 
   if (!questions || !categories) {
     notFound();
@@ -15,6 +18,10 @@ export default async function Page() {
 
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <h1 className={`${lusitana.className} mb-4 text-xl md:text-2xl`}>
+        Questions
+      </h1>
+      <p className="text-lg font-bold">Welcome, {username}!</p>
       <div className='flex flex-col md:flex-row'>
         <div className="flex flex-col gap-6 rounded-lg bg-gray-50 px-6 py-10 md:w-2/5 md:px-20">
           <h1 className="text-[20px]">Categories Table</h1>
